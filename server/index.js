4829a793dd035951605de124a7386590921de011
Jace H <Jaceholdaway@gmail.com>
2018-07-13 13:51:12 -0600

    changes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
+require('dotenv').config()
 const bodyParser = require('body-parser');
 const express = require('express');
 const controller = require('./controller');
+const massive = require('massive');
 
 const app = express();
+
 app.use(bodyParser.json());
+
+massive( process.env.CONNECTION_STRING ).then( dbInstance => {
+    app.set('db', dbInstance)
+}).catch(err => consolel.log(err) );
 
 const port = process.env.SERVER_PORT;
 app.listen(port, () => {
